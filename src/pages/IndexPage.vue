7f4effe87ffc41444a1d878ff3127e9d42734c95
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-02-06 21:28:17 +0900

    🧱build: init project

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-pa-md">
+    <q-carousel
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      :autoplay="autoplay"
+      arrows
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+    >
+      <q-carousel-slide
+        :name="1"
+        img-src="https://atimg.sonyunara.com/files/attrangs/new_banner/1706669368_0.jpg"
+      />
+      <q-carousel-slide
+        :name="2"
+        img-src="https://atimg.sonyunara.com/files/attrangs/new_banner/1706691517_0.jpg"
+      />
+      <q-carousel-slide
+        :name="3"
+        img-src="https://atimg.sonyunara.com/files/attrangs/new_banner/1706520455_0.jpg"
+      />
+    </q-carousel>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+const slide = ref(1);
+const autoplay = ref(true);
+</script>
